1b4d93b830f689a73c882ccefa853d7f6c37a7ee
Ish4L <faishalsaputra58@gmail.com>
2026-03-30 11:17:31 +0800

    added dashboard.php

---
+++ admin/dashboard.php
@@ -0,0 +1,24 @@
+<? include "db.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Dashboard Admin</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header"></div>
+
+        <div class="sidebar">
+            <div class="sidebar-title"><b>JajanD JalanD</b></div>
+            <ul><?php include 'sidebar.php' ?></ul>
+        </div>
+
+        <div class="section">
+            <h2>Selamat Datang Admin Sistem</h2>
+        </div>
+    </div>
+</body>
+</html>
