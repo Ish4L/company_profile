356e3c49768c59633e0221f00ef2f43361bb4105
Ish4L <faishalsaputra58@gmail.com>
2026-04-08 10:40:33 +0800

    added styleadmin.css

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Dashboard Admin</title>
+    <link rel="stylesheet" href="../css/styleadmin.css">
 </head>
 <body>
     <div class="wrapper">
